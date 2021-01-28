ff59645f33eea0f130094f99edb948c7ec6cfc5a
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2021-01-28 16:28:20 +0530

    intial

---
+++ SAKEC_GATE-main/lib/screens/aboutus.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About US"),
+      ),
+      body: Text("About US SAKEC GATE  contact us : mail_id , number  Loaction of sakec"),
+      
+    );
+  }
+}
